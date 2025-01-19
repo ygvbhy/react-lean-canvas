12190296ae4deadbf1b9ac80465a099cc613a100
ygvbhy <ygvbhy@naver.com>
2025-01-19 23:39:55 +0900

    타입 재정의

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,6 +11,8 @@ export interface NoteType {
   content: string;
 }
 
-export interface CanvasSearchParams {
-  title_like: string;
-}
+// 검색 조건 타입
+// 제네릭으로 타입으로 각 캔버스의 항목에 _like 키를 추가 한 타입
+export type CanvasSearchParams = {
+  [K in keyof CanvasItemProps as `${K}_like`]?: string;
+};
