b1b50f110f14a019beb9cc57f4b784dd29df5882
ygvbhy <ygvbhy@naver.com>
2025-01-19 22:59:57 +0900

    json mock server 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchBar from '../components/SearchBar';
 import ViewToggle from '../components/ViewToggle';
 import CanvasList from '../components/canvas/CanvasList';
-import OriginCanvasItemData from '../data/OriginCanvasItem.json';
 import { CanvasItemProps } from '../types';
 
 function Home() {
   const [isGrid, setIsGrid] = useState(true);
   const [searchText, setSearchText] = useState('');
-  const [canvasItemList, setCanvasItemList] =
-    useState<CanvasItemProps[]>(OriginCanvasItemData);
-  // debugger;
-  // 해당 코드가 적힌 부분에서 웹 페이지가 멈추고 디버깅 가능하게 함
+  const [canvasItemList, setCanvasItemList] = useState<CanvasItemProps[]>([]);
+
   const filteredCanvasItemList = canvasItemList.filter((item) =>
     item.title.toLowerCase().includes(searchText.toLowerCase()),
   );
@@ -19,6 +16,18 @@ function Home() {
   const onDeleteItem = (id: number) => {
     setCanvasItemList(canvasItemList.filter((item) => item.id !== id));
   };
+
+  const getFetchCanvasData = async () => {
+    const data = await fetch('http://localhost:8000/canvases')
+      .then((res) => res.json())
+      .catch(console.error);
+
+    setCanvasItemList(data);
+  };
+
+  useEffect(() => {
+    getFetchCanvasData();
+  }, []);
 
   return (
     <div className="container mx-auto px-4 py-16">
